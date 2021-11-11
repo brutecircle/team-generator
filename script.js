70cc437c282d93a6b4a21f6ea6f11e4f7a783094
Talent M <simeontofiakwa@gmail.com>
2021-11-11 15:46:24 +0000

    create a function to split and merge it

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const team = [ "Talent", "Talen", "alen", "Tlen", "Ten",
 let numberOfPeopleInTeams = 3;
 // randomize the team array 
 let randomize = team.sort(() => Math.random() - 0.5);
+
+function genArray(arr, count) {
+    // initialize it to a new empty Array
+  let newArray = [];
+  for (i = 0; i < arr.length; i += count) {
+      maxTeamMember = arr.slice(i, i + count);
+      newArray.push(maxTeamMember);
+    }
+  return newArray;
+}
+let result = genArray(randomize, numberOfPeopleInTeams);
+console.log(result); //team names 
+console.log(result.length) //number of teams ge
