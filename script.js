4cfee8c07ec46a65e1183c8ae86dc4eba843d6b5
Talent M <simeontofiakwa@gmail.com>
2021-11-11 15:45:22 +0000

    create a team array

---
+++ script.js
@@ -0,0 +1,6 @@
+// random team 
+const team = [ "Talent", "Talen", "alen", "Tlen", "Ten", 
+        "Tale", "Taln", "lent", "ant", "Talet" ,"john", "Sara", "Tom"];
+
+// number of people in each team array / teams 
+
